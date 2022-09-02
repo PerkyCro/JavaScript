3618fbc6b4ac2e9df4e11e3e4314b5b966a2cd10
Marin Perak <marin.perak7@gmail.com>
2022-09-02 20:04:07 +0200

    second commit

---
+++ assignments.js
@@ -0,0 +1,13 @@
+const country = "Croatia";
+const continent = "Europe";
+let population = "4000000";
+
+const isIsland = false;
+
+console.log(typeof isIsland);
+
+const language = "Croatian";
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
